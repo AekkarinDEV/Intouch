c27608f4e6791ba16b2d38782c17081e02f8aad8
AekkarinDEV <ezio4227@gmail.com>
2024-11-28 22:25:21 +0700

    done sign up api

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,18 +1,47 @@
-import { redirect } from "next/dist/server/api-utils";
+
 import { NextRequest, NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+import { v4 as uuidv4 } from 'uuid'
+const bcrypt = require("bcrypt");
+
+
+const client = new MongoClient(process.env.MONGO_URI);
 
 
 export  async function POST(req:NextRequest){
-    const body =  await req.formData()
-    const username = body.get("username")
-    const password = body.get("password")
-    return new NextResponse('All ok', {
-        status: 200,
-  });
-}
+  try{
+      const body =  await req.formData()
+      const username:string = body.get("username")
+      let password:string = body.get("password")
+      password= await bcrypt.hash(password,10);
+      console.log(password);
+      await client.connect();
+      const isItExist = await client.db("auth").collection("users").findOne({username: username})
+    console.log(isItExist);
+    if(isItExist !== null && isItExist.username == username){
+      return new NextResponse("user alredy exist",{
+        status:400,
+      }
 
-export async function GET(req:NextRequest){
-  return new NextResponse("halo from api" , {
-    status: 200
-  })
+      )
+    }
+      await client.db("auth").collection("users").insertOne({
+        username: username,
+        password: password,
+        token: await uuidv4()
+      })
+
+
+
+      return new NextResponse('All ok', {
+          status: 200,
+    });
+  }catch(err){
+    console.log(err);
+    return new NextResponse('Somthing wrong with server',{
+      status: 400,
+    })
+  }finally{
+    client.close();
+  }
 }
